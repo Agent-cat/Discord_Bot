fc65fb83cdae13233ab0af6c30b0ceb82098a524
Agent-cat <230031042@kluniversity.in>
2024-12-30 17:52:10 +0530

    Remove deprecated Discord bot command files and related configurations. This includes the deletion of Command.js, index.js, command handlers for 'ping' and 'code', Discord client setup, and response formatting utilities. The README.md file was also updated.

--- dist/commands/ping.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handlePingCommand = void 0;
-const handlePingCommand = async (interaction) => {
-    await interaction.reply("Pong!");
-};
-exports.handlePingCommand = handlePingCommand;
